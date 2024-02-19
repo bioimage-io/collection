4c1ed6a6bbcd5795d0105348fc2f769c077bad76
fynnbe <thefynnbe@gmail.com>
2024-02-19 13:53:36 +0100

    fix test_client

--- tests/test_scripts/test_utils/test_s3client.py
+++ tests/test_scripts/test_utils/test_s3client.py
@@ -1,11 +1,5 @@
-from time import sleep
-
-
 def test_client():
     from scripts.utils.s3_client import Client
-
-    BGM = True
-    """bypass governance mode to remove files immediatly"""
 
     client = Client()
     assert client.prefix.startswith("sandbox")
@@ -13,12 +7,10 @@ def test_client():
     client.put_json("test/dir/test2.json", "test")
     client.put_json("test/dir/test3.json", "test")
     assert set(client.ls("test/")) == {"test1.json", "dir"}
-    client.mv_dir("test/", "test_b/", bypass_governance_mode=BGM)
-    sleep(3)
+    client.mv_dir("test/", "test_b/")
     assert not set(client.ls("test/"))
     assert set(client.ls("test_b/")) == {"test1.json", "dir"}
-    assert set(client.ls("test_b/dir")) == {"test2.json", "test3.json"}
-    client.rm_dir("test_b/dir/", bypass_governance_mode=BGM)
-    sleep(3)
+    assert set(client.ls("test_b/dir/")) == {"test2.json", "test3.json"}
+    client.rm_dir("test_b/dir/")
     assert set(client.ls("test_b/")) == {"test1.json"}
-    client.rm_dir("test/", bypass_governance_mode=BGM)
+    client.rm_dir("test/")
