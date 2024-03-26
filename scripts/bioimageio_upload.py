afd24cfd3971c1691143af99ffef8fa6fef82154
fynnbe <thefynnbe@gmail.com>
2024-03-26 14:22:11 +0100

    add bioimageio_upload.py

---
+++ scripts/bioimageio_upload.py
@@ -0,0 +1,25 @@
+"""
+programmatic staging of a new resource version (for advanced/internal use only)
+"""
+
+import os
+
+import github
+
+
+def bioimageio_upload(resource_id: str, package_url: str):
+    g = github.Github(login_or_token=os.environ["GITHUB_PAT"])
+
+    repo = g.get_repo("bioimage-io/collection")
+
+    workflow = repo.get_workflow("stage.yaml")
+
+    ref = repo.get_branch("main")
+    ok = workflow.create_dispatch(
+        ref=ref,
+        inputs={
+            "resource_id": resource_id,
+            "package_url": package_url,
+        },
+    )
+    assert ok
