fd3e9a3d6949584599da33e6b4c447657892dcb5
fynnbe <thefynnbe@gmail.com>
2024-04-15 23:29:50 +0200

    remove unused arg s3_test_folder_url

--- tests/test_backoffice.py
+++ tests/test_backoffice.py
@@ -2,9 +2,7 @@ from bioimageio_collection_backoffice import BackOffice
 from bioimageio_collection_backoffice._settings import settings
 
 
-def test_backoffice(
-    backoffice: BackOffice, package_url: str, package_id: str, s3_test_folder_url: str
-):
+def test_backoffice(backoffice: BackOffice, package_url: str, package_id: str):
     backoffice.stage(resource_id=package_id, package_url=package_url)
     backoffice.test(resource_id=package_id, version="staged/1")
     backoffice.await_review(resource_id=package_id, version="staged/1")
