8d856167b5392a5734368b848aec7ac992409d0d
fynnbe <thefynnbe@gmail.com>
2024-05-15 15:25:55 +0200

    improve get_conda_env

--- bioimageio_collection_backoffice/validate_format.py
+++ bioimageio_collection_backoffice/validate_format.py
@@ -2,7 +2,7 @@ import warnings
 from typing import Dict, List, Literal, Optional, Tuple, TypedDict, Union, cast
 
 from bioimageio.spec import InvalidDescr, ResourceDescr, load_description
-from bioimageio.spec.common import RelativeFilePath, Sha256
+from bioimageio.spec.common import RelativeFilePath
 from bioimageio.spec.model import v0_4, v0_5
 from bioimageio.spec.model.v0_5 import Version, WeightsFormat
 from bioimageio.spec.summary import ErrorEntry, ValidationDetail
@@ -37,15 +37,54 @@ class CondaEnv(TypedDict):
     dependencies: List[Union[str, PipDeps]]
 
 
+def ensure_min_env(env: CondaEnv, env_name: Optional[str] = None):
+    """update a conda env such that we have bioimageio.core and the collection backoffice available"""
+    if env_name is None:
+        env["name"] = ensure_valid_conda_env_name(env.get("name", ""))
+    else:
+        env["name"] = env_name
+
+    if "name" not in env:
+        env["name"] = "env"
+
+    if "channels" not in env:
+        env["channels"] = []
+
+    if "dependencies" not in env:
+        env["dependencies"] = []
+
+    if "conda-forge" not in env["channels"]:
+        env["channels"].append("conda-forge")
+
+    if "pip" not in env["dependencies"]:
+        env["dependencies"].append("pip")
+
+    pip_section: PipDeps = {"pip": []}
+    for d in env["dependencies"]:
+        if isinstance(d, dict) and "pip" in d:
+            pip_section = d
+            break
+    else:
+        env["dependencies"].append(pip_section)
+
+    if (
+        collection_main := "git+https://github.com/bioimage-io/collection.git@main"
+    ) not in pip_section["pip"]:
+        pip_section["pip"].append(collection_main)
+
+    if (
+        "bioimageio.core" not in env["dependencies"]
+        or "conda-forge::bioimageio.core" not in env["dependencies"]
+        or "bioimageio.core" not in pip_section["pip"]
+    ):
+        env["dependencies"].append("conda-forge::bioimageio.core")
+
+
 def get_base_env():
     return CondaEnv(
         name="env",
-        channels=["conda-forge"],
-        dependencies=[
-            "bioimageio.core",
-            "pip",
-            PipDeps(pip=["git+https://github.com/bioimage-io/collection.git@main"]),
-        ],
+        channels=[],
+        dependencies=[],
     )
 
 
@@ -59,28 +98,25 @@ def get_env_from_deps(
             pip_deps = [
                 d.strip() for d in download(deps.file).path.read_text().split("\n")
             ]
-            conda_env["dependencies"].append({"pip": pip_deps})
-            return conda_env
+            if "bioimageio.core" not in pip_deps:
+                pip_deps.append("bioimageio.core")
 
-        if deps.manager not in ("conda", "mamba"):
+            conda_env["dependencies"].append(PipDeps({"pip": pip_deps}))
+        elif deps.manager not in ("conda", "mamba"):
             raise ValueError(f"Dependency manager {deps.manager} not supported")
-
-        deps_source = (
-            deps.file.absolute if isinstance(deps.file, RelativeFilePath) else deps.file
-        )
-        sha: Optional[Sha256] = None
+        else:
+            deps_source = (
+                deps.file.absolute
+                if isinstance(deps.file, RelativeFilePath)
+                else deps.file
+            )
+            local = download(deps_source).path
+            conda_env = CondaEnv(**yaml.load(local))
     elif isinstance(deps, v0_5.EnvironmentFileDescr):
-        deps_source = deps.source
-        sha = deps.sha256
+        local = download(deps.source).path
+        conda_env = CondaEnv(**yaml.load(local))
     else:
         assert_never(deps)
-
-    local = download(deps_source, sha256=sha).path
-    conda_env = CondaEnv(**yaml.load(local))
-
-    # add bioimageio.core to dependencies
-    if not any(isinstance(d, str) and d.startswith("bioimageio.core") for d in deps):
-        conda_env["dependencies"].append("conda-forge::bioimageio.core")
 
     return conda_env
 
@@ -125,10 +161,12 @@ def get_tf_env(tensorflow_version: Optional[Version]):
         tensorflow_version = max(
             tensorflow_version, Version("1.13")
         )  # tf <1.13 not available anymore
-        conda_env["dependencies"] = [
-            "pip",
-            "python=3.7.*",
-        ]  # tf 1.15 not available for py>=3.8
+        conda_env["dependencies"].extend(
+            [
+                "pip",
+                "python=3.7.*",
+            ]
+        )  # tf 1.15 not available for py>=3.8
         # get bioimageio.core (and its dependencies) via pip as well to avoid conda/pip mix
         # protobuf pin: tf 1 does not pin an upper limit for protobuf,
         #               but fails to load models saved with protobuf 3 when installing protobuf 4.
@@ -197,8 +235,7 @@ def get_conda_env(
     else:
         assert_never(entry)
 
-    conda_env["name"] = ensure_valid_conda_env_name(env_name)
-
+    ensure_min_env(conda_env, env_name)
     return conda_env
 
 
@@ -230,10 +267,7 @@ def prepare_dynamic_test_cases(
                 continue
 
             wf = cast(WeightsFormat, wf)
-            conda_envs[wf] = get_conda_env(
-                entry=entry,
-                env_name=wf,
-            )
+            conda_envs[wf] = get_conda_env(entry=entry, env_name=wf)
             validation_cases.append({"weight_format": wf})
 
     return validation_cases, conda_envs
