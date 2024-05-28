d64b43d89ec428550df1bd149a68d17b4da751e8
fynnbe <thefynnbe@gmail.com>
2024-05-28 14:19:15 +0200

    debug test_resolve_v0_4_deps

--- tests/test_validate_format.py
+++ tests/test_validate_format.py
@@ -1,7 +1,7 @@
 def test_resolve_v0_4_deps():
     from bioimageio_collection_backoffice.validate_format import validate_format_impl
 
-    _, _, conda_envs = validate_format_impl(
+    rd, _, conda_envs = validate_format_impl(
         "https://uk1s3.embassy.ebi.ac.uk/public-datasets/bioimage.io/ambitious-sloth/1/files/rdf.yaml"
     )
-    assert conda_envs
+    assert conda_envs, rd.validation_summary.format()
