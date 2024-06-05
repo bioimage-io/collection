6f7c9299bc546b9d7b5c79c33688bf18bbf08c8d
fynnbe <thefynnbe@gmail.com>
2024-06-05 11:41:11 +0200

    add batch_update remove_colab_badges

---
+++ scripts/batch_update.py
@@ -0,0 +1,34 @@
+from io import BytesIO
+
+from ruyaml import YAML
+
+from bioimageio_collection_backoffice.remote_collection import RemoteCollection
+from bioimageio_collection_backoffice.s3_client import Client
+
+yaml = YAML(typ="safe")
+
+
+def remove_colab_badges():
+    rc = RemoteCollection(Client())
+    for r in rc.get_published_versions():
+        rdf = r.get_rdf()
+        for badge in rdf.get("badges", []):
+            if badge.get("icon") == "colab-badge.svg":
+                badge["icon"] = (
+                    "https://colab.research.google.com/assets/colab-badge.svg"
+                )
+
+        stream = BytesIO()
+        yaml.dump(rdf, stream)
+        data = stream.getvalue()
+        assert data
+        r.client.put(r.rdf_path, BytesIO(data), len(data))
+
+        path = f"{r.folder}files/colab-badge.svg"
+        if list(r.client.ls(path)):
+            r.client.rm(path)
+
+
+if __name__ == "__main__":
+    # remove_colab_badges()  # June 5, 2024
+    pass
