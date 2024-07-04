027e1d781da2a626f05b25c026feffd906a06b5d
carlosuc3m <100329787@alumnos.uc3m.es>
2024-07-04 22:21:57 +0200

    keep iterating

--- scripts/get_java_software_versions.py
+++ scripts/get_java_software_versions.py
@@ -2,25 +2,51 @@ import requests
 from bs4 import BeautifulSoup
 import re
 import datetime
+from typing import Dict, Any
+from packaging.version import Version
 
 DEEPIMAGEJ_UPDATE_SITE_URL = "https://sites.imagej.net/DeepImageJ/plugins/"
 DEEPIMAGEJ_PATTERN = r"DeepImageJ-(\d+\.\d+\.\d+)\.jar-(\d{14})"
+MINIMUM_DIJ_VERSION = Version("3.0.0")
+
+
+def download_file(url: str, local_filename: str):
+    with requests.get(url, stream=True) as response:
+        response.raise_for_status()
+        with open(local_filename, 'wb') as file:
+            for chunk in response.iter_content(chunk_size=8192):
+                _ = file.write(chunk)
+
+def find_associated_jdll_version():
+    return None
+
 
 def get_deepimagej_versions():
     response = requests.get(DEEPIMAGEJ_UPDATE_SITE_URL)
     soup = BeautifulSoup(response.content, 'html.parser')
     links = soup.find_all('a')
-    files = []
+    v_dic: Dict[str, Any] = {}
+    v_dic_rev: Dict[str, str] = {}
     for link in links:
         href = link.get('href')
-        if href.contains('.jar') and href.lower().contains('deepimagej'):
-            files.append(href)
+        if '.jar' in href and 'deepimagej' in href.lower():
+            tmp_dic = get_dij_version_and_date(href)
+            if Version(tmp_dic["vv"]) < MINIMUM_DIJ_VERSION:
+                continue
+            if tmp_dic["vv"] not in v_dic_rev.keys():
+                v_dic_rev[tmp_dic["vv"]] = tmp_dic["ts"]
+                v_dic[href] = tmp_dic
+            elif tmp_dic["ts"] > v_dic_rev[tmp_dic["vv"]]:
+                v_dic[href] = tmp_dic
+                v_dic_rev[tmp_dic["vv"]] = tmp_dic["ts"]
+    find_associated_jdll_version()
+    
 
 
-def get_dij_version_and_date(filename: str):
+def get_dij_version_and_date(filename: str) -> Dict[str, Any]:
     match = re.search(DEEPIMAGEJ_PATTERN, filename)
 
-    version_dic = {}
+    version_dic: Dict[str, Any] = {}
     if match:
         version = match.group(1)
         date = match.group(2)
@@ -29,7 +55,7 @@ def get_dij_version_and_date(filename: str):
     else:
         version_dic["vv"] = None
         version_dic["ts"] = None
-    return version
+    return version_dic
 
     
 
@@ -39,20 +65,4 @@ def get_dij_version_and_date(filename: str):
 # Parse the HTML content
 
 
-# Find all links on the page
-links = soup.find_all('a')
-
-# Extract file details
-files = []
-for link in links:
-    href = link.get('href')
-    if href.endswith('.jar'):  # Filter for jar files
-        file_info = {
-            'name': href,
-            'url': url + href
-        }
-        files.append(file_info)
-
-# Display the list of files
-for file in files:
-    print(f"Name: {file['name']}, URL: {file['url']}")
+get_deepimagej_versions()
