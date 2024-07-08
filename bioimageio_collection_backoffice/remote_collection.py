904e6672b6747b683b552c659ef56d1a35392d80
fynnbe <thefynnbe@gmail.com>
2024-07-08 16:17:51 +0200

    fix get_all_compatibility_reports

--- bioimageio_collection_backoffice/remote_collection.py
+++ bioimageio_collection_backoffice/remote_collection.py
@@ -666,7 +666,7 @@ class RecordBase(RemoteBase, ABC):
             if d.endswith(".json") and (tool is None or d[:-4] == tool)
         ]
         reports_data = {
-            t: self.client.load_file(f"{self.folder}compatibility/{t}")
+            t: self.client.load_file(self.get_compatibility_report_path(t))
             for t in tools
         }
         return [
