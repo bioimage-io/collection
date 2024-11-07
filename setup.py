237f63c8fab0934b96c94c0205d8e0198730c913
fynnbe <thefynnbe@gmail.com>
2024-11-07 17:33:10 +0100

    use bioimageio.spec.get_conda_env

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ _ = setup(
     packages=find_packages(exclude=["tests"]),
     install_requires=[
         "bioimageio.core>=0.6.7",
-        "bioimageio.spec>=0.5.3",
+        "bioimageio.spec>=0.5.3.4",
         "fire",
         "loguru",
         "markdown",
