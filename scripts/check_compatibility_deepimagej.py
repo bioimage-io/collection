a47614cb3dc496644a2bb498091090e5886055fc
carlosuc3m <100329787@alumnos.uc3m.es>
2025-01-28 17:14:58 +0100

    correct error stupid

--- scripts/check_compatibility_deepimagej.py
+++ scripts/check_compatibility_deepimagej.py
@@ -77,7 +77,7 @@ def test_model_deepimagej(rdf_url: str, fiji_executable: str, fiji_path: str):
         stdout=subprocess.PIPE,
         text=True
         )
-        model_dir = out_str.strip().splitlines()[-1]
+        model_dir = download_result.stdout.strip().splitlines()[-1]
     except BaseException as e:
         report = CompatibilityReportDict(
                 status="failed",
