d5593a5a91309fc32e58880bad140f2a5c0899f3
fynnbe <thefynnbe@gmail.com>
2025-09-22 14:09:54 +0200

    requests -> httpx

--- src/backoffice/_config/__init__.py
+++ src/backoffice/_config/__init__.py
@@ -2,11 +2,11 @@ import json
 from functools import lru_cache
 from pathlib import Path
 
-import requests
+import httpx
 from pydantic import HttpUrl
 
-from .._requests_utils import raise_for_status_discretely
 from .._settings import settings
+from ..utils_plain import raise_for_status_discretely
 from .common import ConfigNode
 from .id_parts import IdParts
 from .reviewers import Reviewers
@@ -20,7 +20,7 @@ class CollectionConfig(ConfigNode, frozen=True):
     @lru_cache
     def load(cls):
         if isinstance(settings.collection_config, HttpUrl):
-            r = requests.get(str(settings.collection_config))
+            r = httpx.get(str(settings.collection_config))
             raise_for_status_discretely(r)
             data = r.json()
         else:
